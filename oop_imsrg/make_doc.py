4535973a2e96b988eafbcd091e4af1bd98914c4e
Davison <davison@intranet.nscl.msu.edu>
2019-07-23 14:35:28 -0400

    auto-generate documentation using pydoc

--- oop_imsrg/make_doc.py
+++ oop_imsrg/make_doc.py
@@ -1,18 +1,25 @@
 import os
 import sys
+import glob
 
 doc = ""
-
+mv = ""
+cmd = ""
+# print(os.name)
 if os.name == 'posix':
-	doc_dir = "doc/"
+    doc_dir = "doc/"
+    mv = "mv"
+    # cmd = "{:s} *.html {:s}".format(mv, doc_dir)
 elif os.name == 'nt':
-	doc_dir = "doc\\"
+    doc_dir = "doc\\"
+    mv = "move"
+    # cmd = "{:s} -Path .\\*.html -Destination .\\{:s}".format(mv, doc_dir)
 else:
-	print("Could not detect operating system")
-	exit()
+    print("Could not detect operating system")
+    exit()
 
 if not os.path.exists(doc_dir):
-	os.mkdir(doc_dir)
+    os.mkdir(doc_dir)
 
-os.system('pydoc -w *.py')
-os.system('mv *.html {:s}'.format(doc_dir))
+os.system('pydoc -w hamiltonian flow generator main occupation_tensors plot_data')
+os.system("{:s} *.html {:s}".format(mv, doc_dir))
