58c85b9c8b94ce88fd267a33fa0d1f56eb97fbb6
Davison <davison@intranet.nscl.msu.edu>
2019-08-02 11:27:12 -0400

    finally working

--- oop_imsrg/make_doc.py
+++ oop_imsrg/make_doc.py
@@ -6,16 +6,19 @@ doc = ""
 mv = ""
 cmd = ""
 src = ""
+main_prefix = ""
 # print(os.name)
 if os.name == 'posix':
     src = './oop_imsrg/'
     doc_dir = src+"doc/"
     mv = "mv"
+    main_prefix = "./"
     # cmd = "{:s} *.html {:s}".format(mv, doc_dir)
 elif os.name == 'nt':
     src = '.\\oop_imsrg\\'
     doc_dir = src+"doc\\"
     mv = "move"
+    main_prefix = ".\\"
     # cmd = "{:s} -Path .\\*.html -Destination .\\{:s}".format(mv, doc_dir)
 else:
     print("Could not detect operating system")
@@ -27,7 +30,7 @@ if not os.path.exists(doc_dir):
 os.system('pydoc -w '+src+'hamiltonian.py')
 os.system('pydoc -w '+src+'flow.py')
 os.system('pydoc -w '+src+'generator.py')
-os.system('pydoc -w '+src+'main.py')
+os.system('pydoc -w '+main_prefix+'main.py')
 os.system('pydoc -w '+src+'occupation_tensors.py')
 os.system('pydoc -w '+src+'plot_data.py')
 os.system("{:s} *.html {:s}".format(mv, doc_dir))
